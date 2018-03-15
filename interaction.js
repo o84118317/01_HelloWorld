8b7ed21f55083bfae57c551373eb1b0e97a4ebd0
Chang,Hsi <37285378+o84118317@users.noreply.github.com>
2018-03-15 22:30:55 +0800

    Create interaction.js

---
+++ interaction.js
@@ -0,0 +1,8 @@
+let message = prompt ('How old are you?');
+alert(message);
+
+message = alert ('Yo Watch out!');
+message;
+
+message = confirm ('Just make sure you're grown up!');
+alert(message);
